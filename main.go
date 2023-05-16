ee52f86d63f0912421bbfa3ba3902036a6d356dc
matsuyoshi30 <sfbgwm30@gmail.com>
2023-05-16 22:59:41 +0900

    parse the response

--- main.go
+++ main.go
@@ -3,24 +3,37 @@ package main
 import (
 	"bytes"
 	"encoding/binary"
+	"fmt"
 	"log"
 	"math/rand"
 	"net"
+	"strconv"
 	"strings"
 )
 
-// dnsHeader represents DNS query's header section.
+// DNSHeader represents DNS query's header section.
 // see https://datatracker.ietf.org/doc/html/rfc1035#section-4.1.1
-type dnsHeader struct {
-	transactionId uint16
-	flags         uint16
-	qdCount       uint16 // the number of entries in the question section
-	anCount       uint16 // the number of resource records in the answer section
-	nsCount       uint16 // the number of name server resource records in the authority records section
-	arCount       uint16 // the number of resource records in the additional records section
+type DNSHeader struct {
+	TransactionId uint16
+	Flags         uint16
+	QdCount       uint16 // the number of entries in the question section
+	AnCount       uint16 // the number of resource records in the answer section
+	NsCount       uint16 // the number of name server resource records in the authority records section
+	ArCount       uint16 // the number of resource records in the additional records section
 }
 
-func headerToBytes(header dnsHeader) ([]byte, error) {
+func (dh *DNSHeader) String() string {
+	var sb strings.Builder
+	sb.WriteString(fmt.Sprintf("TransactionId: %d", dh.TransactionId))
+	sb.WriteString(fmt.Sprintf(", Flags: %d", dh.Flags))
+	sb.WriteString(fmt.Sprintf(", QdCount: %d", dh.QdCount))
+	sb.WriteString(fmt.Sprintf(", AnCount: %d", dh.AnCount))
+	sb.WriteString(fmt.Sprintf(", NsCount: %d", dh.NsCount))
+	sb.WriteString(fmt.Sprintf(", ArCount: %d", dh.ArCount))
+	return sb.String()
+}
+
+func headerToBytes(header DNSHeader) ([]byte, error) {
 	buf := new(bytes.Buffer)
 	err := binary.Write(buf, binary.BigEndian, &header)
 	if err != nil {
@@ -29,26 +42,43 @@ func headerToBytes(header dnsHeader) ([]byte, error) {
 	return buf.Bytes(), nil
 }
 
-// dnsQuestion represents DNS query's question section.
-// see https://datatracker.ietf.org/doc/html/rfc1035#section-4.1.2
-type dnsQuestion struct {
-	qname  []byte
-	qtype  uint16
-	qclass uint16
+func parseHeader(reader *bytes.Reader) (*DNSHeader, error) {
+	header := &DNSHeader{}
+	err := binary.Read(reader, binary.BigEndian, header)
+	if err != nil {
+		return nil, err
+	}
+	return header, nil
 }
 
-func questionToBytes(question dnsQuestion) ([]byte, error) {
-	type s struct {
-		qtype  uint16
-		qclass uint16
-	}
-	ss := s{
-		qtype:  question.qtype,
-		qclass: question.qclass,
+// DNSQuestion represents DNS query's question section.
+// see https://datatracker.ietf.org/doc/html/rfc1035#section-4.1.2
+type DNSQuestion struct {
+	Qname []byte
+	subDNSQuestion
+}
+
+type subDNSQuestion struct {
+	Qtype  uint16
+	Qclass uint16
+}
+
+func (dq *DNSQuestion) String() string {
+	var sb strings.Builder
+	sb.WriteString(fmt.Sprintf("name: %s", string(dq.Qname)))
+	sb.WriteString(fmt.Sprintf(", type: %d", dq.Qtype))
+	sb.WriteString(fmt.Sprintf(", class: %d", dq.Qclass))
+	return sb.String()
+}
+
+func questionToBytes(question DNSQuestion) ([]byte, error) {
+	ss := subDNSQuestion{
+		Qtype:  question.Qtype,
+		Qclass: question.Qclass,
 	}
 
 	buf := new(bytes.Buffer)
-	err := binary.Write(buf, binary.BigEndian, question.qname)
+	err := binary.Write(buf, binary.BigEndian, question.Qname)
 	if err != nil {
 		return nil, err
 	}
@@ -61,11 +91,85 @@ func questionToBytes(question dnsQuestion) ([]byte, error) {
 	return buf.Bytes(), nil
 }
 
-// encodeDnsName encodes domainName as QNAME.
+func parseQuestion(reader *bytes.Reader) (*DNSQuestion, error) {
+	name, err := decodeName(reader)
+	if err != nil {
+		return nil, err
+	}
+
+	subDNSQuestion := &subDNSQuestion{}
+	err = binary.Read(reader, binary.BigEndian, subDNSQuestion)
+	if err != nil {
+		return nil, err
+	}
+
+	return &DNSQuestion{Qname: name, subDNSQuestion: *subDNSQuestion}, nil
+}
+
+// DNSRecord represents DNS record.
+// see https://datatracker.ietf.org/doc/html/rfc1035#section-4.1.3
+type DNSRecord struct {
+	Name []byte
+	SubDNSRecord
+	Rdata []byte
+}
+
+type SubDNSRecord struct {
+	Type     uint16
+	Class    uint16
+	Ttl      uint32
+	RdLength uint16
+}
+
+func (dr *DNSRecord) String() string {
+	var sb strings.Builder
+	sb.WriteString(fmt.Sprintf("name: %s", string(dr.Name)))
+	sb.WriteString(fmt.Sprintf(", type: %d", dr.Type))
+	sb.WriteString(fmt.Sprintf(", class: %d", dr.Class))
+	sb.WriteString(fmt.Sprintf(", ttl: %d", dr.Ttl))
+	sb.WriteString(fmt.Sprintf(", rdLength: %d", dr.RdLength))
+	sb.WriteString(fmt.Sprintf(", rdata: %x", dr.Rdata))
+	return sb.String()
+}
+
+func parseRecord(reader *bytes.Reader) (*DNSRecord, error) {
+	name, err := decodeName(reader)
+	if err != nil {
+		return nil, err
+	}
+
+	subDNSRecord := &SubDNSRecord{}
+	err = binary.Read(reader, binary.BigEndian, subDNSRecord)
+	if err != nil {
+		return nil, err
+	}
+
+	data := make([]byte, subDNSRecord.RdLength)
+	_, err = reader.Read(data)
+	if err != nil {
+		return nil, err
+	}
+
+	return &DNSRecord{
+		Name:         name,
+		SubDNSRecord: *subDNSRecord,
+		Rdata:        data,
+	}, nil
+}
+
+func ip2String(ip []byte) string {
+	ips := make([]string, len(ip))
+	for i, v := range ip {
+		ips[i] = strconv.Itoa(int(v))
+	}
+	return strings.Join(ips, ".")
+}
+
+// encodeDNSName encodes domainName as QNAME.
 //
 // a domain name represented as a sequence of labels, where each label consists of a length octet followed by that
 // number of octets.  The domain name terminates with the zero length octet for the null label of the root.
-func encodeDnsName(domainName string) ([]byte, error) {
+func encodeDNSName(domainName string) ([]byte, error) {
 	buf := new(bytes.Buffer)
 	for _, part := range strings.Split(domainName, ".") {
 		if err := binary.Write(buf, binary.BigEndian, uint8(len(part))); err != nil {
@@ -81,33 +185,154 @@ func encodeDnsName(domainName string) ([]byte, error) {
 	return buf.Bytes(), nil
 }
 
+func decodeName(reader *bytes.Reader) ([]byte, error) {
+	var parts []string
+
+	for {
+		length, err := reader.ReadByte()
+		if err != nil {
+			return nil, err
+		}
+		if length == 0 {
+			break
+		}
+
+		// see https://datatracker.ietf.org/doc/html/rfc1035#section-4.1.4
+		if length&0b11000000 == 0b11000000 {
+			compressedParts, err := decodeCompressedName(length, reader)
+			if err != nil {
+				return nil, err
+			}
+			parts = append(parts, string(compressedParts))
+			break
+		}
+
+		part := make([]byte, length)
+		_, err = reader.Read(part)
+		if err != nil {
+			return nil, err
+		}
+
+		parts = append(parts, string(part))
+	}
+
+	return []byte(strings.Join(parts, ".")), nil
+}
+
+func decodeCompressedName(length uint8, reader *bytes.Reader) ([]byte, error) {
+	next, err := reader.ReadByte()
+	if err != nil {
+		return nil, err
+	}
+
+	pointer := int(length&0x3F)<<8 | int(next)
+	currentPos := reader.Size() - int64(reader.Len())
+	reader.Seek(int64(pointer), 0)
+
+	compressedPart, err := decodeName(reader)
+	if err != nil {
+		return nil, err
+	}
+
+	// Return the pointer to the original position
+	reader.Seek(currentPos, 0)
+
+	return compressedPart, nil
+}
+
+// DNSPacket represents DNS packet.
+// see https://datatracker.ietf.org/doc/html/rfc1035#section-4.1
+type DNSPacket struct {
+	Header      *DNSHeader
+	Questions   []*DNSQuestion
+	Answers     []*DNSRecord
+	Authorities []*DNSRecord
+	Additionals []*DNSRecord
+}
+
+func parsePacket(data []byte) (*DNSPacket, error) {
+	reader := bytes.NewReader(data)
+
+	header, err := parseHeader(reader)
+	if err != nil {
+		return nil, err
+	}
+
+	questions := make([]*DNSQuestion, header.QdCount)
+	for i := range questions {
+		question, err := parseQuestion(reader)
+		if err != nil {
+			return nil, err
+		}
+		questions[i] = question
+	}
+
+	answers := make([]*DNSRecord, header.AnCount)
+	for i := range answers {
+		answer, err := parseRecord(reader)
+		if err != nil {
+			return nil, err
+		}
+		answers[i] = answer
+	}
+
+	authorities := make([]*DNSRecord, header.AnCount)
+	for i := range authorities {
+		authority, err := parseRecord(reader)
+		if err != nil {
+			return nil, err
+		}
+		authorities[i] = authority
+	}
+
+	additionals := make([]*DNSRecord, header.AnCount)
+	for i := range additionals {
+		additional, err := parseRecord(reader)
+		if err != nil {
+			return nil, err
+		}
+		additionals[i] = additional
+	}
+
+	return &DNSPacket{
+		Header:      header,
+		Questions:   questions,
+		Answers:     answers,
+		Authorities: authorities,
+		Additionals: additionals,
+	}, nil
+}
+
 const (
 	TYPE_A   = 1
 	CLASS_IN = 1
 )
 
 func buildQuery(domainName string, recordType uint16) ([]byte, error) {
-	name, err := encodeDnsName(domainName)
+	name, err := encodeDNSName(domainName)
 	if err != nil {
 		return nil, err
 	}
 	id := rand.Intn(65535)
 	RECURSION_DESIRED := 1 << 8
 
-	header := dnsHeader{
-		transactionId: uint16(id),
-		flags:         uint16(RECURSION_DESIRED),
-		qdCount:       1,
+	header := DNSHeader{
+		TransactionId: uint16(id),
+		Flags:         uint16(RECURSION_DESIRED),
+		QdCount:       1,
 	}
 	headerBytes, err := headerToBytes(header)
 	if err != nil {
 		return nil, err
 	}
 
-	question := dnsQuestion{
-		qname:  name,
-		qtype:  recordType,
-		qclass: CLASS_IN,
+	subDNSQuestion := subDNSQuestion{
+		Qtype:  recordType,
+		Qclass: CLASS_IN,
+	}
+	question := DNSQuestion{
+		Qname:          name,
+		subDNSQuestion: subDNSQuestion,
 	}
 	questionBytes, err := questionToBytes(question)
 	if err != nil {
@@ -118,7 +343,7 @@ func buildQuery(domainName string, recordType uint16) ([]byte, error) {
 }
 
 func run() {
-	query, err := buildQuery("www.example.com", TYPE_A)
+	query, err := buildQuery("example.com", TYPE_A)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -139,15 +364,14 @@ func run() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	packet, err := parsePacket(response)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(ip2String(packet.Answers[0].Rdata))
 }
 
 func main() {
-	run()
-	// % sudo tcpdump -ni any port 53
-	// Password:
-	// tcpdump: data link type PKTAP
-	// tcpdump: verbose output suppressed, use -v[v]... for full protocol decode
-	// listening on any, link-type PKTAP (Apple DLT_PKTAP), snapshot length 524288 bytes
-	// 22:20:17.785047 IP 192.168.0.32.60529 > 8.8.8.8.53: 62929+ A? www.example.com. (33)
-	// 22:20:17.809896 IP 8.8.8.8.53 > 192.168.0.32.60529: 62929 1/0/0 A 93.184.216.34 (49)
+	run() // 93.184.216.34
 }
